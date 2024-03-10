51fe8116568a8b520f46a614a3eb756279733ea6
jayapramanik <jayakumari22dec@gmail.com>
2024-03-10 16:06:56 +0530

    query and event-bus

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-const CommentList = ({ postId }) => {
-    const [comments, setComments] = useState([]);
-
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    }
-
-    useEffect(() => {
-        fetchComments();
-    }, []);
-
+import React from 'react';
+const CommentList = ({ comments }) => {
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+        if(comment.status === 'approved'){
+            content = comment.content
+        }
+        else if(comment.status === 'pending'){
+            content = `This comment is awaiting moderation`;
+        }
+        else if(comment.status === 'rejected') {
+            content = `This comment has been rejected`;
+        }
+        return <li key={comment.id}>{content}</li>
     })
 
 
